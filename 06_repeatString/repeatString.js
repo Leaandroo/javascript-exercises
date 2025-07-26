d3c6d5b9ffb76968799daad4ca8c3ad9cc33e030
Leaandroo <Leandrozapata2002@gmail.com>
2025-07-26 03:14:11 +0000

    Loops & Arrays exercises finished

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (word, index) {
+	let total = "";
+	if (index >= 0) {
+		for (let i = 0; i < index; i++) {
+			total += word;
+		}
+		return total;
+	}
+	return "ERROR";
 };
 
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degress) {
+	let conversion = ((degress - 32) * 5) / 9;
+
+	return +conversion.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degress) {
+	let conversion = degress * (9 / 5) + 32;
+
+	return +conversion.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
